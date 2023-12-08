ca572e01bdf2abc7d8e3515bd814ddf9f7fcf8e1
macieryann <ryanmacie@yahoo.com>
2023-12-08 11:16:41 -0600

    added typing effect to the intro title

--- portfolio-ui/src/components/intro.js
+++ portfolio-ui/src/components/intro.js
@@ -3,7 +3,10 @@ import * as React from 'react';
 export default function Intro() {
     return (
         <div className="intro-padding">
-            <h1 className="intro-typography">Hi, I'm Macie Ryan <br /> Software Engineer</h1>
+            <div className="typing-effect">
+                <h1 className="intro-typography">Hi, I'm Macie Ryan <br /> Software Engineer</h1>
+            </div>
         </div>
+
     );
 }
