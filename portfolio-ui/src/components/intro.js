bd3e536a421e18d110df7b457401b33492a596e4
macieryann <ryanmacie@yahoo.com>
2023-12-13 20:03:24 -0600

    made typing effect happen every 30 seconds and added some details to the project section and an old project

--- portfolio-ui/src/components/intro.js
+++ portfolio-ui/src/components/intro.js
@@ -1,12 +1,25 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Intro() {
+    useEffect(() => {
+        const intervalId = setInterval(restartTypingEffect, 30000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    function restartTypingEffect() {
+        const element = document.querySelector('.typing-effect');
+        if (element) {
+            element.classList.remove('restart-typing');
+            void element.offsetWidth;
+            element.classList.add('restart-typing');
+        }
+    }
+
     return (
         <div className="intro-padding">
             <div className="typing-effect">
                 <h1 className="intro-typography">Hi, I'm Macie Ryan <br /> Software Engineer</h1>
             </div>
         </div>
-
     );
 }
--- portfolio-ui/src/components/projects.js
+++ portfolio-ui/src/components/projects.js
@@ -1,18 +1,42 @@
 import { Card, Container } from '@mui/material';
 import * as React from 'react';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 
 export default function Projects() {
 
     return (
         <Container className="component-padding">
-            <Card className="resume-card">
-                <h1>My Projects (many are on-going!)</h1>
-                <h3 className="left-align">My React Porfolio</h3>
+            <Card className="projects-card">
+                <h1>My Projects (they're on-going!)</h1>
+                {/* GITHUB */}
+
+                <h3 className="left-align">
+                    <GitHubIcon style={{ marginRight: '5px' }} />
+                    <a href="https://github.com/macieryann/" target="_blank" rel="noreferrer">GitHub</a>
+                </h3>
+                {/* HABIT TRACKER */}
+                <h3 className="left-align">
+                    <AddTaskIcon style={{ marginRight: '5px' }} />
+                    <a href="https://github.com/macieryann/habit-tracker" target="_blank" rel="noreferrer">
+                        Habit Tracker
+                    </a>
+                </h3>
+                <p className="paragraph">Full stack application (Java backend / React frontend) that allows users to track habits by the week.  I started this project in order to kick-start my own weekly habits.  I wanted a habit tracker that met some specific "requirements" of my own so I thought: what better way than to create my own.</p>
+                {/* PORTFOLIO */}
+                <h3 className="left-align">
+                    <EmojiPeopleIcon style={{ marginRight: '5px' }} />
+                    <a href="https://github.com/macieryann/macieryan" target="_blank" rel="noreferrer">React Portfolio Github</a>
+                </h3>
                 <p className="paragraph">You're looking at it!  I created this Portfolio to work on my ReactJS skills and also just to have an updated site with all of my information.</p>
-                <a className="left-align" href="https://github.com/macieryann/macieryan" target="_blank" rel="noreferrer">Portfolio Github Link</a>
-                <h3 className="left-align">Habit Tracker (Java)</h3>
-                <a className="left-align" href="https://github.com/macieryann/habit-tracker" target="_blank" rel="noreferrer">Java Habit Tracker</a>
-                <p></p>
+                {/* PRODUCTION PROJECT */}
+                <h3 className="left-align">
+                    <PrecisionManufacturingIcon style={{ marginRight: '5px' }} />
+                    <a href="https://github.com/macieryann/ProductionProject-Official" target="_blank" rel="noreferrer">Facility Production Project</a>
+                </h3>
+                <p className="paragraph">The Production Project was created in my Object-Oriented Programming course during my senior year at Florida Gulf Coast University.  This program is for a media player production facility to keep tracking of products being produced.  With this program, employees of the production facility can input new products and store them in a collection that can be recalled by any other employees at any time.</p>
             </Card>
         </Container>
     );
